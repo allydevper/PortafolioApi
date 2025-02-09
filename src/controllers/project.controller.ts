4c0a4b3f7331c2e2a433192b7e87df40ae56c79b
allydevper <alamaswilmer@gmail.com>
2025-02-08 23:40:26 -0500

    update controller

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,26 +1,33 @@
 import { Project } from '../models/project.interface';
+import { createClient, PostgrestError } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_KEY!;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export const getProjects = async (): Promise<Project[]> => {
-    return [
-        {
-            id: 1,
-            name: 'Proyecto 1',
-            url: 'http://proyecto1.com',
-            description: 'Descripción del Proyecto 1',
-            technologies: ['JavaScript', 'Node.js'],
-            create_date: new Date('2023-01-01')
-        },
-        {
-            id: 2,
-            name: 'Proyecto 2',
-            url: 'http://proyecto2.com',
-            description: 'Descripción del Proyecto 2',
-            technologies: ['TypeScript', 'Express'],
-            create_date: new Date('2023-02-01')
-        }
-    ];
+    const { data, error } = await supabase
+        .from('projects')
+        .select('*');
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return data as Project[];
 };
 
-export const createProject = async (project: Project): Promise<Project> => {
-    return project;
+export const createProject = async (project?: Project): Promise<PostgrestError | null> => {
+    const { error } = await supabase
+        .from('projects')
+        .insert([project]);
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return error;
 }; 
