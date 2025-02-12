69738b50c66a06087833778d0db4fdc27e38dcb8
allydevper <alamaswilmer@gmail.com>
2025-02-12 16:20:21 -0500

    update cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import projectRoutes from './routes/project.routes';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:4321', 'https://alamadev.onrender.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
