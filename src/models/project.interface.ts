8cfecf0d7b5bc84aa1cc9ee325b0d135208c0bba
allydevper <alamaswilmer@gmail.com>
2025-02-12 23:38:00 -0500

    Update project.interface.ts

--- src/models/project.interface.ts
+++ src/models/project.interface.ts
@@ -1,7 +1,8 @@
 export interface Project {
     id?: number;
     name: string;
-    url?: string;
+    url_project?: string;
+    url_demo?: string;
     description?: string;
     technologies?: string[];
 } 
